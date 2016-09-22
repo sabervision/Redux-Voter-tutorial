83eb71367d1552a02c892668f72c4b3067b40aaa
sabervision <brhernandez314@gmail.com>
2016-09-21 18:15:03 -0700

    updated core logic for voting and getWinners, formatting

--- test/core_spec.js
+++ test/core_spec.js
@@ -129,7 +129,7 @@ describe ('application logic', () => {
 				}),
 				entries: List()
 			});
-			
+				
 			const nextState = vote(state, 'Trainspotting');
 			expect(nextState).to.equal(Map({
 				vote: Map({
