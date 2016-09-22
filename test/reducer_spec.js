be338aa9eb1fe2939d55024ccc01d823061be36d
sabervision <brhernandez314@gmail.com>
2016-09-22 01:16:57 -0700

    added reducer and reducer_spec

---
+++ test/reducer_spec.js
@@ -0,0 +1,27 @@
+import { Map, fromJS } from 'immutable';
+import { expect } from 'chai';
+
+import reducer from '../src/reducer';
+
+describe ('reducer', () => {
+
+	it ('handle SET_ENTRIES', () => {
+
+		const initialState = Map();
+		const action = {type: 'SET_ENTRIES', entries: ['Trainspotting']};
+		const nextState = reducer(initialState, action);
+
+		expect(nextState).to.equal(fromJS({
+			entries: ['Trainspotting']
+		}));
+	});
+
+	it ('handles next', () => {
+
+		const initialState = fromJS({
+			entries: ['Trainspotting', '28 Days Later']
+		});
+		const action = {type: 'NEXT'};
+		const nextState = reducer(initialState, action);
+	})
+});
